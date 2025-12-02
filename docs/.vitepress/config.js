be27e4a20ea99e790a6167347d608cbc8367acbe
千夏蝉时雨 <limoxuan190@gmail.com>
2025-12-02 15:02:34 +0800

    feat: my personal website

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+  title: "千夏蝉时雨的开发笔记",
+  description: "前端开发者 | React | TypeScript",
+
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于我', link: '/about/' },
+      { text: '项目', link: '/projects/' }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Ashleykawaii233' }
+    ],
+    sidebar: false
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+
+hero:
+  name: "千夏蝉时雨"
+  text: "全栈web开发者"
+  tagline: "专注 React · TypeScript · 开源"
+  actions:
+    - theme: brand
+      text: 我的项目
+      link: /projects/
+    - theme: alt
+      text: 关于我
+      link: /about/
+---
